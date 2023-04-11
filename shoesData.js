2ccbccaa4af8662a193f0e7c255390ed82cb6667
Ayush Yadav <ayushcs.net24@gmail.com>
2023-04-11 21:49:41 +0530

    //

--- shoesData.js
+++ shoesData.js
@@ -8,79 +8,79 @@ var product = [{
 }, {
     "id": 1,
     "title": "Samsung Galaxy S22",
-    "price": 179999,
+    "price": 1650,
     "description": "Samsung Galaxy S22 price in Pakistan is Rs. 179,999. Official dealers and warranty providers regulate the retail price of Samsung mobile products in official warranty.",
-    "brand": "Samsung ",
+    "brand": "Daniel klein ",
     "image": "https://www.whatmobile.com.pk/admin/images/Samsung/SamsungGalaxyS22-b.jpg"
 }, {
     "id": 2,
     "title": "Vivo X80",
-    "price": 159999,
+    "price": 9645,
     "description": "Vivo X80 price in Pakistan is Rs. 159,999. Official dealers and warranty providers regulate the retail price of Vivo mobile products in official warranty.",
-    "brand": "Vivo",
+    "brand": "Titan",
     "image": "https://www.whatmobile.com.pk/admin/images/Vivo/VivoX80-b.jpg"
 }, {
     "id": 3,
     "title": "Oppo F21 Pro 5G",
     "price": 69999,
     "description": "Oppo F21 Pro 5G price in Pakistan is Rs. 69,999. Official dealers and warranty providers regulate the retail price of Oppo mobile products in official warranty.",
-    "brand": "Oppo",
+    "brand": "Fastrack",
     "image": "https://www.whatmobile.com.pk/admin/images/Oppo/OppoF21Pro5G-b.jpg"
 }, {
     "id": 4,
     "title": "Infinix Note 12",
     "price": 36999,
     "description": "Infinix Note 12 price in Pakistan is Rs. 36,999. Official dealers and warranty providers regulate the retail price of Infinix mobile products in official warranty.",
-    "brand": "Infinix",
+    "brand": "Sonata",
     "image": "https://www.whatmobile.com.pk/admin/images/Infinix/InfinixNote12-b.jpg"
 }, {
     "id": 5,
     "title": "Samsung Galaxy A13",
     "price": 35499,
     "description": "Samsung Galaxy A13 price in Pakistan is Rs. 35,499. Official dealers and warranty providers regulate the retail price of Samsung mobile products in official warranty.",
-    "brand": "Samsung",
+    "brand": "CASIO",
     "image": "https://www.whatmobile.com.pk/admin/images/Samsung/SamsungGalaxyA13-b.jpg"
 }, {
     "id": 6,
     "title": "Xiaomi 12",
     "price": 179999,
     "description": "Xiaomi 12 price in Pakistan is Rs. 179,999. Official dealers and warranty providers regulate the retail price of Xiaomi mobile products in official warranty.",
-    "brand": "Xiaomi",
+    "brand": "Mast&Harbour",
     "image": "https://www.whatmobile.com.pk/admin/images/Xiaomi/Xiaomi12-b.jpg"
 }, {
     "id": 7,
     "title": "Huawei Nova 9",
     "price": 107999,
     "description": "Huawei Nova 9 price in Pakistan is Rs. 107,999. Official dealers and warranty providers regulate the retail price of Huawei mobile products in official warranty.",
-    "brand": "Huawei",
+    "brand": "Tommy Hilfiger",
     "image": "https://www.whatmobile.com.pk/admin/images/Huawei/HuaweiNova9-b.jpg"
 }, {
     "id": 8,
     "title": "Realme C35",
     "price": 32999,
     "description": "Realme C35 price in Pakistan is Rs. 32,999. Official dealers and warranty providers regulate the retail price of Realme mobile products in official warranty.",
-    "brand": "Realme",
+    "brand": "Boat",
     "image": "https://www.whatmobile.com.pk/admin/images/Realme/RealmeC35-b.jpg"
 }, {
     "id": 9,
     "title": "Tecno Spark 8C 3GB",
     "price": 20499,
     "description": "Tecno Spark 8C 3GB price in Pakistan is Rs. 20,499. Official dealers and warranty providers regulate the retail price of Tecno mobile products in official warranty.",
-    "brand": "Tecno",
+    "brand": "Calvin Klein",
     "image": "https://www.whatmobile.com.pk/admin/images/Tecno/TecnoSpark8C3GB-b.jpg"
 }, {
     "id": 10,
     "title": "itel Vision 3",
     "price": 16299,
     "description": "itel Vision 3 price in Pakistan is Rs. 16,299. Official dealers and warranty providers regulate the retail price of itel mobile products in official warranty.",
-    "brand": "Itel",
+    "brand": "DUCATI CORSE",
     "image": "https://www.whatmobile.com.pk/admin/images/itel/itelVision3-b.jpg"
 },
 {
     "id":11,
     "title":"Motorola g52",
     "price":15799,
-    "brand":"Motorola",
+    "brand":"Lee Cooper",
     "image":"https://cdn1.smartprix.com/rx-iVj5YXk6O-w1200-h1200/Vj5YXk6O.jpg"
 },
 {
@@ -94,7 +94,7 @@ var product = [{
     "id":13,
     "title":"OnePlus 8 NORD 5G, OnePlus Z",
     "price":24999,
-    "brand":"OnePlus",
+    "brand":"Noise",
     "image":"https://www.gizmochina.com/wp-content/uploads/2020/07/OnePlus-Nord-1-600x600.jpg"
 }
 ]
